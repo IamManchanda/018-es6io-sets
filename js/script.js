f5a33a01bfff28ecd221fa375306969b8219a12b
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-14 14:00:05 +0530

    Sets Intro!

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * JS
+ */
+
+/* eslint-disable */
+const people = new Set();
+people.add('Harry');
+people.add('Snickers');
+people.add('Kait');
+
+for (const person of people) {
+  console.log(person);
+}
+
+const students = new Set(['Wes', 'Kara', 'Tony']);
+const dogs = ['Snickers', 'Sunny']
+const dogSet = new Set(dogs);
+
+
